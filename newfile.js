0be48e604e90afdf3aa4d4ccdc6227f8d94f9761
leeyuan <dbdks32123@gmail.com>
2023-10-11 10:54:25 +0900

    for문으로 배열 속 객체 생성

--- newfile.js
+++ newfile.js
@@ -8,6 +8,13 @@ let test = new student("공욱재", "0");
 console.log(test);
 // 객체를 뽑아냄
 
+let list = ["이유안", "방승희"];
+let studentArr = [];
+for(let i = 0; i < list.length; i++){
+  studentArr[i] = new student(list[i], i+1)
+}
+console.log(studentArr);
+
 // let studentList = {
 //   name : "hello",
 //   number :
